bf0f84da1ee558f985b682d5ca829ecc8a429328
David <davidrimoh@gmail.com>
2020-08-26 18:58:22 +0300

    midway in displaying all tasks list in all task page

--- app/src/main/java/com/example/ive/logic/Task.java
+++ app/src/main/java/com/example/ive/logic/Task.java
@@ -8,14 +8,21 @@ public class Task {
     private String m_taskID;
     private String m_Title = "";
     private int m_urgency;
-    private int m_estimatedDuration;
-    private Location m_location;
+    private int m_estimatedDurationMinutes;
+    private Location m_location ;
     //optional//
     private Date m_dueDate;
     private String m_Description = "";
 
-    public Task(String i_title, int i_urgency,int i_estimatedDuration) {
+    public Task(String i_title, int i_urgency, int i_estimatedDurationMinutes, Location i_location) {
+        m_Title = i_title;
+        m_urgency = i_urgency;
+        m_estimatedDurationMinutes = i_estimatedDurationMinutes;
+        m_location = i_location;
     }
 
 
+    public String getTitle() {
+        return m_Title;
+    }
 }
