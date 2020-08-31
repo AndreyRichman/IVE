ab5385e3173d9057cec2b3e998dbc9c600f52904
Andrey <andrey.richman@gmail.com>
2020-09-01 01:27:39 +0300

    remove top action bar

--- app/src/main/java/com/example/ive/MainActivity.java
+++ app/src/main/java/com/example/ive/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ive.pages.AllTasksActivity;
-//import com.example.ive.pages.HomeActivity;
+import com.example.ive.pages.HomeActivity;
 import com.example.ive.pages.LobbyActivity;
 import com.example.ive.pages.LoginActivity;
 import com.example.ive.pages.NewTaskActivity;
@@ -44,9 +44,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(allTasksPage);
     }
 
-//    public void goMainPage(View btn){
-//        Intent homePage = new Intent(this, HomeActivity.class);
-//        startActivity(homePage);
-//    }
+    public void goMainPage(View btn){
+        Intent homePage = new Intent(this, HomeActivity.class);
+        startActivity(homePage);
+    }
 
 }
--- app/src/main/java/com/example/ive/pages/HomeActivity.java
+++ app/src/main/java/com/example/ive/pages/HomeActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.ive.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -21,11 +21,11 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
