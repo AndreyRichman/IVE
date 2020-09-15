02cc00f8fedee6b5e1cfb6a9701e5c5ec969e04b
David <davidrimoh@gmail.com>
2020-09-16 01:39:50 +0300

    created get location, returns null for now

---
+++ app/src/main/java/com/example/ive/taskManager/TaskManager.java
@@ -0,0 +1,33 @@
+package com.example.ive.taskManager;
+
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import com.example.ive.Task.Task;
+
+import java.util.List;
+
+public class TaskManager  {
+    public List<Task> getAllTasks(String userID) {
+return null;    }
+
+    ;
+
+    public Task getTask(String taskID)
+    {return null;    }
+
+    ;
+
+    public void updateTask(Task task, String userID) {    }
+
+    ;
+
+    public void addTask(Task task, String userID) {
+    }
+
+
+
+    ;
+}
