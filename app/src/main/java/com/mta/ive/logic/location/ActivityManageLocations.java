eda9a61ab0fd2db96596b26a5613e22a03943fe4
Shaked Lerer <shakedlerer@gmail.com>
2020-09-16 00:59:58 +0300

    fixing missing bar at "manage locations page" icons creating *need to add these icons to bottom bar

--- app/src/main/java/com/mta/ive/logic/location/ActivityManageLocations.java
+++ app/src/main/java/com/mta/ive/logic/location/ActivityManageLocations.java
@@ -1,5 +1,6 @@
 package com.mta.ive.logic.location;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,8 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.mta.ive.R;
+import com.mta.ive.pages.home.home.AddLocationFragment;
 import com.mta.ive.pages.login.SignUpInActivity;
 
 
@@ -26,8 +29,42 @@ public class ActivityManageLocations extends AppCompatActivity
         addLocationBtn = (Button)findViewById(R.id.add_location_button);
         addLocationBtn.setOnClickListener(v ->
         {
-            Intent addLocationIntent = new Intent(this, ActivityAddNewLocation.class);
+            Intent addLocationIntent = new Intent(this, AddLocationFragment.class);
             startActivity(addLocationIntent);
         });
+        setNavigationButtons();
+
     }
+
+    private void setNavigationButtons() {
+        ((BottomNavigationView)findViewById(R.id.nav_view)).setSelectedItemId(R.id.navigation_home);
+
+        findViewById(R.id.navigation_location).setOnClickListener( t -> {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("selection", "1");
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        });
+        findViewById(R.id.navigation_home).setOnClickListener( t -> {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("selection", "2");
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        });
+        findViewById(R.id.navigation_add).setOnClickListener( t -> {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("selection", "3");
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        });
+
+        findViewById(R.id.navigation_user).setOnClickListener( t -> {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("selection", "4");
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+        });
+    }
+
+
 }
