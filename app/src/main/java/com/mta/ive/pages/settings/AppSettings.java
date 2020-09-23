5288b4519e24a5027150178e3fc009035de1592f
Andrey <andrey.richman@gmail.com>
2020-09-23 23:37:44 +0300

    settings: add validation of selected times

--- app/src/main/java/com/mta/ive/pages/settings/AppSettings.java
+++ app/src/main/java/com/mta/ive/pages/settings/AppSettings.java
@@ -3,28 +3,37 @@ package com.mta.ive.pages.settings;
 import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mta.ive.R;
+
+import java.time.LocalTime;
 import java.util.Calendar;
 
 public class AppSettings extends AppCompatActivity {
 
     TextView startTime, endTime;
+    LocalTime userStartTime, userEndTime;
+    Button saveSettingsButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_app_settings);
 
         startTime = findViewById(R.id.startTime);
         endTime = findViewById(R.id.endTime);
+        saveSettingsButton = findViewById(R.id.save_app_settings_button);
 
         startTime.setOnClickListener(click -> {
             Calendar mcurrentTime = Calendar.getInstance();
@@ -32,14 +41,14 @@ public class AppSettings extends AppCompatActivity {
             int minute = mcurrentTime.get(Calendar.MINUTE);
 
             TimePickerDialog mTimePicker = new TimePickerDialog(AppSettings.this, new TimePickerDialog.OnTimeSetListener() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                    String hour = selectedHour < 10? "0" + selectedHour : String.valueOf(selectedHour);
-                    String minute = selectedMinute < 10? "0" + selectedMinute : String.valueOf(selectedMinute);
-                    startTime.setText(hour + ":" + minute);
+                    userStartTime = LocalTime.MIN.plusHours(selectedHour).plusMinutes(selectedMinute);
+                    startTime.setText("Day starts at " +userStartTime.toString());
                 }
             }, hour, minute, true);//Yes 24 hour time
-            mTimePicker.setTitle("Select Time");
+//            mTimePicker.setTitle("Select beginning of day time");
             mTimePicker.show();
         });
 
@@ -49,16 +58,29 @@ public class AppSettings extends AppCompatActivity {
             int minute = mcurrentTime.get(Calendar.MINUTE);
 
             TimePickerDialog mTimePicker = new TimePickerDialog(AppSettings.this, new TimePickerDialog.OnTimeSetListener() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                    String hour = selectedHour < 10? "0" + selectedHour : String.valueOf(selectedHour);
-                    String minute = selectedMinute < 10? "0" + selectedMinute : String.valueOf(selectedMinute);
-                    endTime.setText(hour + ":" + minute);
+                    userEndTime = LocalTime.MIN.plusHours(selectedHour).plusMinutes(selectedMinute);
+                    endTime.setText("Day ends at " + userEndTime.toString());
                 }
             }, hour, minute, true);//Yes 24 hour time
-            mTimePicker.setTitle("Select Time");
+//            mTimePicker.setTitle("Select end of day time");
             mTimePicker.show();
         });
+
+        saveSettingsButton.setOnClickListener(click -> {
+            if (userStartTime.isAfter(userEndTime)){
+                String msg = "Beginning of day must be before end of day";
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+            else{
+                String msg = "Settings Saved";
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
         setNavigationButtons();
     }
 
