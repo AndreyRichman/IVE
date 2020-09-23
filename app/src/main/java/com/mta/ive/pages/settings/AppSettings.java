9f6d0c69ad2e133fab61f5040a46c282922b2243
Andrey <andrey.richman@gmail.com>
2020-09-23 22:50:58 +0300

    settings: add start & end time pickers

--- app/src/main/java/com/mta/ive/pages/settings/AppSettings.java
+++ app/src/main/java/com/mta/ive/pages/settings/AppSettings.java
@@ -1,26 +1,64 @@
 package com.mta.ive.pages.settings;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mta.ive.R;
-import com.mta.ive.pages.home.home.AddLocationFragment;
-import com.mta.ive.vm.adapter.LocationsAdapter;
-
-import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AppSettings extends AppCompatActivity {
 
+    TextView startTime, endTime;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_app_settings);
+
+        startTime = findViewById(R.id.startTime);
+        endTime = findViewById(R.id.endTime);
+
+        startTime.setOnClickListener(click -> {
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+
+            TimePickerDialog mTimePicker = new TimePickerDialog(AppSettings.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    String hour = selectedHour < 10? "0" + selectedHour : String.valueOf(selectedHour);
+                    String minute = selectedMinute < 10? "0" + selectedMinute : String.valueOf(selectedMinute);
+                    startTime.setText(hour + ":" + minute);
+                }
+            }, hour, minute, true);//Yes 24 hour time
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+        });
+
+        endTime.setOnClickListener(click -> {
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+
+            TimePickerDialog mTimePicker = new TimePickerDialog(AppSettings.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    String hour = selectedHour < 10? "0" + selectedHour : String.valueOf(selectedHour);
+                    String minute = selectedMinute < 10? "0" + selectedMinute : String.valueOf(selectedMinute);
+                    endTime.setText(hour + ":" + minute);
+                }
+            }, hour, minute, true);//Yes 24 hour time
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+        });
         setNavigationButtons();
     }
 
